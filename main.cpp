4d23b441a670a69e9e39e1345c24fc080f3e02f4
aoyako <ml.logachevksy@gmail.com>
2022-12-28 20:36:40 +0900

    Transfer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+void ErrorCallback(int, const char* err_str) {
+  std::cout << "GLFW Error: " << err_str << std::endl;
+}
+
 int main() {
-  std::cout << "Hello, world!" << std::endl;
+  glfwSetErrorCallback(ErrorCallback);
+  if (!glfwInit()) {
+    std::cout << "b" << std::endl;
+    return 1;
+  }
+
+  const char* glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+  std::cout << "a" << std::endl;
   return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,10 @@
 add_rules("mode.release", "mode.debug")
 
+add_requires("imgui", {configs = {glfw_opengl3 = true}})
+
 target("chip8")
     set_kind("binary")
     add_files("main.cpp")
+    add_packages("imgui")
+    add_links("pthread")
+    add_links("dl")
