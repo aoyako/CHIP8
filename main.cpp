934b6d9511f29c6a763758265b8b6ed15adfeeaa
aoyako <ml.logachevsky@gmail.com>
2023-02-25 00:44:37 +0900

    Add ROM load

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 #include <SDL2/SDL_mixer.h>
 #include <stdio.h>
 
+#include <iostream>
 #include <stdexcept>
 
 #include "src/device.hpp"
 
 int main(int argc, char* argv[]) {
-  Device device("roms/invaders.ch8");
+  if (argc < 2) {
+    std::cerr << "provide a chip8 ROM" << std::endl;
+    return EXIT_FAILURE;
+  }
+  Device device(argv[1]);
   device.run();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
